8048b9a30691d2c7b35f458518fdd5915bc6f7e1
Joey <joey.frimin@gmail.com>
2023-12-05 20:28:28 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 4200;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
